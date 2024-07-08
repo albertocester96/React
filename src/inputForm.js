8644d4a60f73de4c49d603fcd6b60ac7a1199cd4
albertocester96 <albertocester96@gmail.com>
2024-07-08 12:25:05 +0200

    it-works

--- src/inputForm.js
+++ src/inputForm.js
@@ -2,38 +2,62 @@ import { useState } from "react";
 
 const InputForm = () => {
 
+const [data, setData] = useState(null)
+
 function handleSubmit(e) {
     
     e.preventDefault();
+    
 
     const form = e.target;
     const formData = new FormData(form)
+    const data = {
+        name: formData.get("name"),
+        email: formData.get("email"),
+        photo: formData.get("photo")
+    }
     
-    fetch('https://jsonplaceholder.typicode.com/users', { method: form.method, body: formData} )
+    fetch('https://jsonplaceholder.typicode.com/users', { method: form.method, body: JSON.stringify(data)} )
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then((responseData) => {
+        console.log(responseData);
+        setData(data);
+    })
     .catch(error => console.error("Errore durante la richiesta:", error));
     
     
 };
 
     return (
+    <>
     <form method="POST" onSubmit={handleSubmit}>
         <div className="input-form">
             <label>
-                Nome <input type="text" autoFocus={true} placeholder=" Inserisci il tuo nome" />
+                Nome <input type="text" name="name" autoFocus={true} placeholder=" Inserisci il tuo nome" />
             </label>
             <label>
-                Email <input type="email" autoFocus={true} placeholder=" Inserisci la tua email" />
+                Email <input type="email" name="email" autoFocus={true} placeholder=" Inserisci la tua email" />
             </label>
             <label>
-                Foto <input type="file" autoFocus={true} placeholder=" Inserisci la tua foto" />
+                Foto <input type="file" name="photo" autoFocus={true} placeholder=" Inserisci la tua foto" />
             </label>
             <button type="submit">
                 Registra
             </button>
         </div>
     </form>
+
+    <div> 
+        {data && (
+            <div>
+                <h1>{data.name}</h1>
+                <p> {data.email} </p>
+            </div>
+        )}
+        
+    </div>
+
+    </>
     )
 };
 
